79d1c54291cabbd222cb2f2a8b146af9e23c1fec
RossanaPalma <70126235+RossanaPalma@users.noreply.github.com>
2021-10-11 21:31:56 -0700

    Add project files.

---
+++ W7Lab/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace W7Lab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculationButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int Operation = listBox.SelectedIndex;
+                decimal leftHand = Convert.ToDecimal(leftTextBox.Text);
+                decimal rightHand = Convert.ToDecimal(rightTextBox.Text);
+                decimal result = 0;
+                switch (Operation)
+                {
+                    case 0:
+                        result = leftHand + rightHand;
+                        resultTextBox.Text = result.ToString();
+                        break;
+                    case 1:
+                        result = leftHand - rightHand;
+                        resultTextBox.Text = result.ToString();
+                        break;
+                    case 2:
+                        result = leftHand * rightHand;
+                        resultTextBox.Text = result.ToString();
+                        break;
+                    case 3:
+                        result = leftHand / rightHand;
+                        resultTextBox.Text = result.ToString();
+                        break;
+                    case 4:
+                        result = leftHand % rightHand;
+                        resultTextBox.Text = result.ToString();
+                        break;
+                    default:
+                        result = 0;
+                        MessageBox.Show("No operation selected");
+                        break;
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("error : Can not divide by zero");
+            }
+        }
+    }
+}
